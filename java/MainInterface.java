c9e52d8d9ce38db805139d00a4b588f88bbba944
Evelyn Zhiyuan Tang <ttang99@tamu.edu>
2020-09-24 23:00:24 -0500

    add edge class, finish database_meta, add edge related functions and test them, keep track of visited nodes and edge list

--- java/MainInterface.java
+++ java/MainInterface.java
@@ -9,6 +9,32 @@ import java.sql.*;
 import java.util.*;
 import java.lang.*;
 
+
+class Edge{
+  public String s1;
+  public String s2;
+  public Integer edgelabel = 0;
+  public Edge(){
+    s1 = "";
+    s2 = "";
+  }
+  public Edge(String t1, String t2){
+    s1 = t1;
+    s2 = t2;
+  }
+  public boolean equaledge(Edge e){
+    if((e.s1).equals(this.s1) && (e.s2).equals(this.s2)){
+      return true;
+    }
+    return false;
+  }
+
+  public void printEdge(){
+    System.out.println("s1:"+s1+" s2:"+s2+" "+"edgelabel:"+edgelabel);
+  }
+};
+
+
 public class MainInterface {
 
     // driver and docker url
@@ -20,7 +46,36 @@ public class MainInterface {
     static final String USER = "root";
     static final String PASS = "password";
 
+
+    static Map<String,ArrayList<String>> adj_list_by_column = new HashMap<String,ArrayList<String>>();
+    static Map<String,ArrayList<String>> adj_list_by_table = new HashMap<String,ArrayList<String>>();
+    static ArrayList<Edge> column_name = new ArrayList<Edge>(); // store visied info
+    static ArrayList<String> table_name = new ArrayList<String>();
+    static Map<String,Integer> visitedNode = new HashMap<String,Integer>();
+    // static Map<Edge,Integer> visitedEdge = new HashMap<String,Integer>();
+    static Stack<String> stk = new Stack<String>();
     // main loop to log into and interact with database
+
+    public static ArrayList<Edge> reset_edge(ArrayList<Edge> edgelist){
+      for(int i = 0; i < edgelist.size();i++){
+        edgelist.get(i).edgelabel = 0; // set to unexplored
+      }
+      return edgelist;
+    }
+
+    public static boolean find_edge(ArrayList<Edge> edgelist, Edge e){
+      boolean found = false;
+      for(int i = 0; i < edgelist.size();i++){
+        if(edgelist.get(i).equaledge(e)){
+          found = true;
+          break;
+        }
+      }
+      return found;
+    }
+
+
+
     public static void main(String[] args) {
         Connection conn = null;
         Statement stmt = null;
@@ -41,7 +96,44 @@ public class MainInterface {
             Scanner sc = new Scanner(System.in);
             boolean loop = true;
 
-            database_meta(conn,stmt);
+
+// ------------ DO NOT DELETE THE CODE BELOW -------------------------
+
+            // test cases passed find_edge and reset_edge
+            Edge e1 = new Edge("n1","n2");
+            Edge e2 = new Edge("n3","n2");
+            Edge e3 = new Edge("n2","n4");
+            Edge e4 = new Edge("n3","n5");
+            Edge e5 = new Edge("n1","n4");
+            ArrayList<Edge> sampleEdgelist = new ArrayList<Edge>(Arrays.asList(e1,e2,e3,e4,e5));
+
+
+            e2.edgelabel = 1;
+            e5.edgelabel = 1;
+            for(int i =0; i < sampleEdgelist.size();i++){
+              sampleEdgelist.get(i).printEdge();
+            }
+
+            System.out.println(find_edge(sampleEdgelist,e2));
+
+            sampleEdgelist = reset_edge(sampleEdgelist);
+            for(int i =0; i < sampleEdgelist.size();i++){
+              sampleEdgelist.get(i).printEdge();
+            }
+
+
+
+
+
+
+
+
+            // database_meta(conn,stmt,adj_list_by_column,adj_list_by_table,table_name,column_name);
+
+            // intialized , none of them is visited ------------ move this into while loop later
+            for(int i = 0; i < table_name.size();i++){
+              visitedNode.put(table_name.get(i),0);
+            }
 
 
             //
@@ -134,12 +226,12 @@ public class MainInterface {
     }// end main
 
     // helper function build a map
-    public static void database_meta(Connection conn, Statement stmt){
+    public static void database_meta(Connection conn, Statement stmt,Map<String,ArrayList<String>> adj_list_by_column, Map<String,ArrayList<String>> adj_list_by_table, ArrayList<String> table_name, ArrayList<Edge> column_name){
       try{
         // ArrayList<ArrayList<String>> adjmatx = new ArrayList<ArrayList<String>>();
-        ArrayList<String> column_name = new ArrayList<String>(); // if not convinient, change to map
+
         ArrayList<ArrayList<String>> tbl_col = new ArrayList<ArrayList<String>>();
-        Map<String,ArrayList<String>> adj_list_by_column = new HashMap<String,ArrayList<String>>();
+        // Map<String,Map<String,Integer>> matrix = new HashMap<String,HashMap<String,Integer>>();
         // Integer count = 0;
         String tablename;
 
@@ -151,7 +243,7 @@ public class MainInterface {
         while (tables.next()) {
          tablename = tables.getString("TABLE_NAME");
          // table_name_map.put(tablename,count);
-         column_name.add(tablename);
+         table_name.add(tablename);
         }
 
         // checking
@@ -159,8 +251,8 @@ public class MainInterface {
         //   System.out.println(column_name.get(i));
         // }
 
-        System.out.println("***************************************");
-        System.out.println("***************************************");
+        // System.out.println("***************************************");
+        // System.out.println("***************************************");
 
         String query1 = "select table_name,column_name from information_schema.columns where table_schema = 'adventureworks' order by table_name,column_name";
 
@@ -186,8 +278,6 @@ public class MainInterface {
         //   System.out.println();
         // }
 
-
-
         // building adj list
         for(int i=0;i<tbl_col.size();i++){
           // ith item's 2nd column
@@ -207,16 +297,73 @@ public class MainInterface {
             temp2.add(curr_tbl); // append
             adj_list_by_column.replace(colstr,temp2); // update
             // System.out.println(colstr + temp2.size());
-
           }
         }
 
+        // // print for checking
+        // for(String k: adj_list_by_column.keySet()){
+        //   System.out.println("******************* "+ k +" ******************");
+        //   for(int j=0;j<adj_list_by_column.get(k).size();j++){
+        //     System.out.println(adj_list_by_column.get(k).get(j));
+        //   }
+        // }
 
-        // print for checking
-        for(String k: adj_list_by_column.keySet()){
-          System.out.println("******************* "+ k +" ******************");
-          for(int j=0;j<adj_list_by_column.get(k).size();j++){
-            System.out.println(adj_list_by_column.get(k).get(j));
+        // building map for table: adj_list_by_table
+        for(int i=0;i<tbl_col.size();i++){
+          // ith item's 2nd column
+          String colstr = tbl_col.get(i).get(1);
+          String curr_tbl = tbl_col.get(i).get(0);
+
+          if(!colstr.toLowerCase().endsWith("id")){
+            // System.out.println("Does not contain");
+            continue;
+          }
+
+          // not being added
+          if(adj_list_by_table.get(curr_tbl)==null){
+            // ith item's 1st column, make a new array
+            ArrayList<String> temp2 = new ArrayList<String>();
+            temp2.add(colstr);
+            adj_list_by_table.put(curr_tbl,temp2);
+            temp2 = null;
+          } else {
+            // System.out.println(adj_list.get(colstr).getClass
+            // System.out.println(curr_tbl);
+            ArrayList<String> temp2 = adj_list_by_table.get(curr_tbl);
+            temp2.add(colstr); // append
+            adj_list_by_table.replace(curr_tbl,temp2); // update
+            // System.out.println(curr_tbl +temp2.size());
+            temp2 = null;
+          }
+        }
+
+        // System.out.println("-------------------------------------------------------------");
+        // System.out.println("-----------------------------then table--------------------------------");
+        //
+        // for(String k: adj_list_by_table.keySet()){
+        //   System.out.println("******************* "+ k +" ******************");
+        //   for(int j=0;j<adj_list_by_table.get(k).size();j++){
+        //     System.out.println(adj_list_by_table.get(k).get(j));
+        //   }
+        // }
+
+        // // initialize matrix
+        // for(int i=0; i < column_name.size();i++){
+        //   String row_table = column_name.get(i);
+        //   for(int j = 0; j < column_name.size();j++){
+        //     String table_temp = column_name.get(j);
+        //     Map<String,Integer> temp3 = new HashMap<String,Integer>()
+        //     temp3.put(table_temp,0);
+        //     matrix.put(row_table,temp3);
+        //   }
+        // }
+
+
+        // get edges
+        for(String k : adj_list_by_column.keySet()){
+          ArrayList<String> temp5 = adj_list_by_column.get(k);
+          for(int j = 0; j < temp5.size();j++){
+            column_name.add(new Edge(k,temp5.get(j)));
           }
         }
 
@@ -234,21 +381,37 @@ public class MainInterface {
 
 
     // search path, print out path
-    public ArrayList<String> search_path(String tb1, String tb2){
+    public void path_finding_DFS(Map<String,ArrayList<String>> adj_list_by_column, Map<String,ArrayList<String>> adj_list_by_table,String tb_start, String tb_end,Map<String,Integer> visitedNode,ArrayList<Edge> column_name,Stack<String> stk){
       try{
+        // set viisted
+        visitedNode.replace(tb_start,1);
+        stk.push(tb_start);
+
+        if((tb_start.toLowerCase()).equals((tb_end.toLowerCase()))){
+          return; // return to previous level
+        }
+
+        ArrayList<String> node_s_columes = adj_list_by_table.get(tb_start);
+        for(int j=0;j<node_s_columes.size();j++){
+          Edge curr_edge = new Edge(tb_start,node_s_columes.get(j));
+          // if(curr_edge.equaledge())
+        }
+
 
 
 
       } catch(Exception e){
-
+        System.out.println("Something wrong in search_path() function");
+        e.printStackTrace();
       }
-      return new ArrayList<String>(Arrays.asList("howdy"));
+
+      return;
     }// end search_path
 
     // search and join tables
     public ArrayList<String> get_views(String view_name, String view_content){
 
-      return new ArrayList<String>(Arrays.asList("howdy"));
+      return new ArrayList<String>();
     }
 
 
