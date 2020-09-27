37ac77de7027cbf3c8098bdc27f6c6fbaabb1cfd
julieherrick <julieherrick@tamu.edu>
2020-09-27 17:59:22 -0500

    update find-column and show-all-primary-keys

--- java/MainInterface.java
+++ java/MainInterface.java
@@ -23,8 +23,8 @@ public class MainInterface {
 
 	// main loop to log into and interact with database
 	public static void main(String[] args) {
-		Connection conn = null; 
-		Statement stmt = null; 
+		Connection conn = null;
+		Statement stmt = null;
 		try {
 			// Connection stuff
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -61,7 +61,7 @@ public class MainInterface {
 					} else {
 						matches.add(m.group()); // unquoted
 					}
-				} 
+				}
 				String parsed_command[] = matches.toArray(new String[matches.size()]);
 
 				switch (parsed_command[0]) {
@@ -72,13 +72,47 @@ public class MainInterface {
 					loop = false;
 					break;
 				case "jdb-show-related-tables":
+				// Given the table <table-name>, list all other tables that have in their
+				// column one or more of the primary keys of the table <table-name>.
+				// SHOW KEYS FROM <table name> WHERE Key_name = 'PRIMARY'
 					System.out.println("jdb-show-related-tables");
+					if (parsed_command.length != 2) {
+						System.out.println("Incorrect amount of arguments");
+						break;
+					}
+					t_name = parsed_command[1];
 					break;
 				case "jdb-show-all-primary-keys":
-					System.out.println("Show primary keys");
+				// Show all primary keys from all tables. Print the list of (table_name, column_name).
+				// SHOW tables
+				// SHOW KEYS FROM (table name) WHERE Key_name = 'PRIMARY'
+					if (parsed_command.length != 1) {
+						System.out.println("Incorrect amount of arguments");
+						break;
+					}
+
+					query = "SELECT Table_name, Column_name FROM INFORMATION_SCHEMA.COLUMNS
+					WHERE TABLE_SCHEMA = 'adventureworks' AND Key_name = 'PRIMARY'";
+					tables = stmt.executeQuery(query);
+					for (int i = 0; i < tables.length; i++) {
+						System.out.println(tables[i]);
+					}
+
 					break;
 				case "jdb-find-column":
-					System.out.println("Find column");
+				// Find all tables that have <column-name>
+					if (parsed_command.length != 2) {
+						System.out.println("Incorrect amount of arguments");
+						break;
+					}
+					c_name = parsed_command[1];
+
+					query = "SELECT Table_name FROM INFORMATION_SCHEMA.COLUMNS
+					WHERE TABLE_SCHEMA = 'adventureworks' AND COLUMN_NAME LIKE '%" + c_name +"'%";
+					tables = stmt.executeQuery(query);
+					for (int i = 0; i < tables.length; i++) {
+						System.out.println(tables[i]);
+					}
 					break;
 				case "jdb-search-path":
 					System.out.println("Search path");
@@ -86,18 +120,125 @@ public class MainInterface {
 				case "jdb-search-and-join":
 					System.out.println("Search and join");
 					break;
-				case "jdb-get-view": 
+				case "jdb-get-view":
 					System.out.println("Get view");
 					break;
 				case "jdb-stat":
-					System.out.println("jdb-stat");
+					if (parsed_command.length != 3) {
+						System.out.println("Incorrect amount of arguments");
+						break;
+					}
+
+					// get index of the column chosen in command
+					rs = stmt.executeQuery("SELECT column_name FROM information_schema.columns where table_name='" + parsed_command[1] + "'");
+					int index_of_column = 0;
+					boolean name_found = false;
+					while (rs.next()) {
+						String colName = rs.getString(1);
+						if (colName.equalsIgnoreCase(parsed_command[2])) {
+							name_found = true;
+							break;
+						}
+						index_of_column++;
+					}
+					if (!name_found) {
+						System.out.println("Table or Column name not found.");
+						break;
+					}
+
+					// make sure data type is numeric
+					rs = stmt.executeQuery("SELECT data_type FROM information_schema.columns where table_name='" + parsed_command[1] + "'");
+					rs.next();
+					for (int i = 0; i < index_of_column; i++) {
+						rs.next();
+					}
+					String datatype = rs.getString(1);
+					if (!(datatype.contains("int") || datatype.contains("dec") || datatype.contains("num") || datatype.contains("float") || datatype.contains("double"))) {
+						System.out.println("Column must contain a numeric data type.");
+						break;
+					}
+
+					// now that query is valid, generate a list of the values in that column
+					rs = stmt.executeQuery("SELECT " + parsed_command[2] + " FROM " + parsed_command[1]);
+					List<Double> list = new ArrayList<Double>();
+					while (rs.next()) {
+						list.add(rs.getDouble(1));
+					}
+
+					// going through list and getting stats
+					double min = list.get(0);
+					double max = list.get(0);
+					double sum = 0;
+					double median = 0;
+					if (list.size()%2 == 0)
+						median = (list.get(list.size()/2) + list.get(list.size()/2 - 1))/2;
+					else
+						median = list.get(list.size()/2);
+
+					for (int i = 0; i < list.size(); i++) {
+						if (list.get(i) < min)
+							min = list.get(i);
+						if (list.get(i) > max)
+							max = list.get(i);
+						sum += list.get(i);
+					}
+					double avg = sum/list.size();
+
+					System.out.format("Min value: %.2f\n", min);
+					System.out.format("Max value: %.2f\n", max);
+					System.out.format("Average: %.2f\n", avg);
+					System.out.format("Median: %.2f\n", median);
+
+					// plotting histogram
+					// first get number of bins and bin width
+					int num_bins = (int)Math.ceil(Math.sqrt(list.size()));
+					double bin_width = (max - min)/num_bins;
+					int[] bins = new int[num_bins];
+
+					// bins[i] = (min + i*bin_width) - (min + (i+1)*bin_width)
+					// populating each bin and scaling them properly
+					int max_count = 0;
+					for (int index = 0; index < list.size(); index++) { // populating each bin with quantity in that bin
+						int i = (int)(Math.floor((list.get(index) - min)/bin_width));
+						i = Math.min(i, bins.length - 1);
+						bins[i]++;
+						if (bins[i] > max_count)
+							max_count = bins[i];
+					}
+					int y_scale = 1;
+					while (max_count/y_scale > 50) // get scale for y axis
+						y_scale *= 10;
+					for (int i = 0; i < bins.length; i++) { // divide each quantity in bins by y_scale
+						bins[i] = (int)Math.round(bins[i]/(double)y_scale);
+					}
+
+					// displaying bins
+					// printing y-axis
+					int offset = String.format("%.2f", max).length() * 2 + 3; // how long the header for each bin should be when displaying
+					System.out.println();
+					System.out.format("%-" + (offset + 1) + "s", "");
+					int max_count_scaled = max_count/y_scale;
+					for (int count = 0; count <= max_count_scaled; count++) // printing y-axis labels
+						System.out.print(count + "___");
+					System.out.format("(Each star and each y-axis label represents %d counts)\n", y_scale);
+
+					// printing each bin
+					for (int i = 0; i < bins.length; i++) {
+						String range = String.format("%.2f - %.2f", min + i*bin_width, min + (i+1)*bin_width);
+						System.out.format("%-" + offset + "s |", range);
+						for (int j = 0; j < bins[i]; j++) {
+							System.out.print("*");
+						}
+						System.out.println();
+					}
+
 					break;
 				case "jdb-customer-info": // jdb-customer-info "conditions" groupby? columnName // displays individual customers line by line that match "conditions", can group by column name, e.g. how many customers in this state
 					createTempAggregateSalesTables(stmt);
 					query = "SELECT ";
-					boolean counting = (parsed_command.length == 3 && parsed_command[1].equalsIgnoreCase("groupby")) 
+					boolean counting = (parsed_command.length == 3 && parsed_command[1].equalsIgnoreCase("groupby"))
 							|| parsed_command.length == 4 && (parsed_command[2].equalsIgnoreCase("groupby"));
-					
+
 					if (counting) {
 						query += parsed_command[parsed_command.length - 1] + ",Count(DISTINCT(customer.CustomerID)) ";
 					} else {
@@ -106,7 +247,7 @@ public class MainInterface {
 								+ "customeraggregate.TotalProducts AS 'Total number of products', subtotals.TotalSpent AS 'Total amount spent', "
 								+ "AddressLine1 AS 'Address Line 1', AddressLine2 AS 'Address Line 2', City, stateprovince.Name AS State, countryregion.Name AS Country, PostalCode AS 'Postal Code' ";
 					}
-					
+
 					// Getting location info from customers
 					query += "FROM customeraggregate "
 							+ "INNER JOIN subtotals ON (customeraggregate.CustomerID=subtotals.CustomerID) "
@@ -115,16 +256,16 @@ public class MainInterface {
 							+ "INNER JOIN address ON (customeraddress.AddressID=address.AddressID) "
 							+ "INNER JOIN stateprovince ON (address.StateProvinceID=stateprovince.StateProvinceID) "
 							+ "INNER JOIN countryregion ON (stateprovince.CountryRegionCode=countryregion.CountryRegionCode) ";
-					
+
 					if (parsed_command.length > 1 && !parsed_command[1].equalsIgnoreCase("groupby")) {
 						query += "WHERE " + parsed_command[1] + " ";
 					}
-					
+
 					if (counting) {
 						query += "GROUP BY " + parsed_command[parsed_command.length - 1] + " ";
 					}
 					//query += "LIMIT 1000";
-					
+
 					rs = stmt.executeQuery(query);
 					printResults(rs);
 					break;
@@ -136,7 +277,7 @@ public class MainInterface {
 					if (parsed_command.length > 1 && !parsed_command[1].toLowerCase().contains("aggregate")) {
 						where_expr = "WHERE " + parsed_command[1] + " ";
 					}
-					if ((parsed_command.length == 2 && parsed_command[1].equalsIgnoreCase("aggregate")) || 
+					if ((parsed_command.length == 2 && parsed_command[1].equalsIgnoreCase("aggregate")) ||
 						 (parsed_command.length == 3 && parsed_command[2].equalsIgnoreCase("aggregate"))) {
 						createTempAggregateSalesTables(stmt); // tables with aggregate sales data for a customer
 						query = "SELECT customer.AccountNumber AS 'Account Number', "
@@ -149,7 +290,7 @@ public class MainInterface {
 								+ "INNER JOIN subtotals ON (customer.CustomerID=subtotals.CustomerID) "
 								+ "INNER JOIN customeraggregate ON (customer.CustomerID=customeraggregate.CustomerID) ";
 					}
-					else if ((parsed_command.length == 2 && parsed_command[1].equalsIgnoreCase("aggregate-by-sales")) || 
+					else if ((parsed_command.length == 2 && parsed_command[1].equalsIgnoreCase("aggregate-by-sales")) ||
 							 (parsed_command.length == 3 && parsed_command[2].equalsIgnoreCase("aggregate-by-sales"))) {
 						createTempAggregateSalesTables(stmt); // tables with aggregate sales data for a customer
 						query = "SELECT customer.AccountNumber AS 'Account Number', "
@@ -172,15 +313,15 @@ public class MainInterface {
 								 + "INNER JOIN salesorderdetail ON (salesorderheader.SalesOrderID=salesorderdetail.SalesOrderID) "
 								 + "INNER JOIN product ON (salesorderdetail.ProductID=product.ProductID) ";
 					}
-					
-					
+
+
 					query += from_expr + where_expr + group_expr + "";
-					
+
 					System.out.println(query);
 					rs = stmt.executeQuery(query);
 					printResults(rs);
 					break;
-				default: // basic sql commands 
+				default: // basic sql commands
 					rs = stmt.executeQuery(command);
 					printResults(rs);
 					break;
@@ -215,17 +356,17 @@ public class MainInterface {
 	public static boolean validateCommand(String command) {
 		command = command.toUpperCase();
 		return !(command.contains("CREATE") || command.contains("DROP") ||
-				command.contains("ALTER") || command.contains("DELETE") || 
+				command.contains("ALTER") || command.contains("DELETE") ||
 				command.contains("INSERT"));
 	}
-	
+
 	public static void printResults(ResultSet rs) throws SQLException {
 		ResultSetMetaData rsmd = rs.getMetaData();
 		int cols = rsmd.getColumnCount();
 
 		// Printing column names
 		for (int i = 1; i <= cols; i++) {
-			if (i > 1) 
+			if (i > 1)
 				System.out.print(", ");
 			System.out.print(rsmd.getColumnLabel(i));
 		}
@@ -234,7 +375,7 @@ public class MainInterface {
 		// Printing query contents
 		while (rs.next()) {
 			for (int i = 1; i <= cols; i++) {
-				if (i > 1) 
+				if (i > 1)
 					System.out.print(", ");
 				String colVal = rs.getString(i);
 				if (colVal.contains(","))
@@ -245,7 +386,7 @@ public class MainInterface {
 			System.out.println("");
 		}
 	}
-	
+
 	public static void createTempAggregateSalesTables(Statement stmt) throws SQLException {
 		stmt.executeUpdate("CREATE TEMPORARY TABLE IF NOT EXISTS subtotals "
 						 + "SELECT salesorderheader.CustomerID, SUM(salesorderheader.SubTotal) AS TotalSpent "
