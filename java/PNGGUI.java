b3481a2d413abd7d014b7a5bdbf82a29b50aaca7
Evelyn Zhiyuan Tang <ttang99@tamu.edu>
2020-10-05 16:26:52 -0500

    instaed of opening the image using imageICON, switch to default desktop image viewer app

--- java/PNGGUI.java
+++ java/PNGGUI.java
@@ -22,28 +22,36 @@ import java.util.*;
 
 public class PNGGUI extends JFrame{
 
-  private ImageIcon img_icon1;
-  // private JPanel panel1;
-  private JLabel img_label;
-  private BufferedImage img_buf;
-  private final int width_base = 1200;
-  private final int height_base = 1000;
+  // private ImageIcon img_icon1;
+  // // private JPanel panel1;
+  // private JLabel img_label;
+  // private BufferedImage img_buf;
+  // private final int width_base = 1200;
+  // private final int height_base = 1000;
 
   public PNGGUI(String img){
     try{
 
-      this.setSize(width_base,height_base);
-      this.setTitle("Graph Schema - jdb-get-schema");
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      img_icon1 = new ImageIcon(img);
-      img_label = new JLabel(img_icon1);
-      add(img_label);
+      // this.setSize(width_base,height_base);
+      // this.setTitle("Graph Schema - jdb-get-schema");
+      // this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      // img_icon1 = new ImageIcon(img);
+      // img_label = new JLabel(img_icon1);
+      // add(img_label);
 
-      this.setVisible(true);
+      // this.setVisible(true);
+
+      File f = new File(img);
+      Desktop desk = Desktop.getDesktop();
+      desk.open(f);
+      JOptionPane.showMessageDialog(null,"Default image viewer opened");
 
     }
     catch(Exception e){
-      System.out.println("******** Something wrong in constructor PNGGUI **********");
+      JOptionPane.showMessageDialog(null,"your default image viewer in your system might not be able to open the image");
+      JOptionPane.showMessageDialog(null,"See more details in terminals");
+      System.out.println("************* error occur when trying to open image *************");
+      System.out.println("******** printing stack trace ***************");
       e.printStackTrace();
     }
   }
